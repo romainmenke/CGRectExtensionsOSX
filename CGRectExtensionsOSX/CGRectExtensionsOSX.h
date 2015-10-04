5a4610bf13cf304bc9feb5cffa112a61da312dd3
Romain Menke <romainmenke@gmail.com>
2015-10-04 05:58:20 +0200

    Initial Commit

---
+++ CGRectExtensionsOSX/CGRectExtensionsOSX.h
@@ -0,0 +1,19 @@
+//
+//  CGRectExtensionsOSX.h
+//  CGRectExtensionsOSX
+//
+//  Created by Romain Menke on 04/10/15.
+//  Copyright © 2015 menke dev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CGRectExtensionsOSX.
+FOUNDATION_EXPORT double CGRectExtensionsOSXVersionNumber;
+
+//! Project version string for CGRectExtensionsOSX.
+FOUNDATION_EXPORT const unsigned char CGRectExtensionsOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CGRectExtensionsOSX/PublicHeader.h>
+
+
